dc322d71948f958881cb165d0425d88b292eba43
m <mustafagokce009@gmail.com>
2021-12-09 22:46:13 +0300

    tu_video: create low latency and high throughput pub/sub services for video streaming

--- tu_settings.py
+++ tu_settings.py
@@ -1,10 +1,5 @@
-tu_vehicle_ip = "localhost"
+tu_vehicle_ip = "192.168.1.201"
 tu_video_stream_port = 2609
-tu_video_stream_fps_max = 30
-tu_video_stream_initial_delay = 0.0
-tu_video_fps_limit = True
-tu_video_frame_width = 1920
-tu_video_frame_height = 1080
-tu_video_frame_channel = 3
-tu_video_stream_timeout = 0.5
-tu_video_capture_device_id = 0
+tu_video_stream_fps = 30
+tu_video_stream_width = 1280
+tu_video_stream_height = 720
