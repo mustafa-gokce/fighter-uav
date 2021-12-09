110905cf46223bb155d5b4127d46f930e67d3c96
m <mustafagokce009@gmail.com>
2021-12-09 15:27:40 +0300

    tu_test: worked on tests

--- tu_test/tu_test_sitl.sh
+++ tu_test/tu_test_sitl.sh
@@ -5,7 +5,7 @@ mkdir -p "$HOME"/test-ucusu/fighter-uav/logs
 cd "$HOME"/test-ucusu/fighter-uav/logs || exit 1
 
 # start simulations
-for i in $(seq 0 0)
+for i in $(seq 0 4)
 do
 	screen -S simulation_firmware$((i + 1)) -d -m bash -c "$HOME/test-ucusu/fighter-uav/tu_test/arduplane -S -I$i --model plane --speedup 1 --defaults $HOME/test-ucusu/fighter-uav/tu_test/plane.parm"
 	sleep 1
--- tu_test/tu_test_telem.sh
+++ tu_test/tu_test_telem.sh
@@ -4,8 +4,8 @@
 mkdir -p "$HOME"/test-ucusu/fighter-uav/logs
 cd "$HOME"/test-ucusu/fighter-uav/logs || exit 1
 
-# start telemetry streams
-for i in $(seq 0 0)
+# start telemetry stream
+for i in $(seq 0 4)
 do
 	screen -S simulation_proxy$((i + 1)) -d -m bash -c "mavproxy.py --aircraft uav$((i + 1)) --master tcp:127.0.0.1:$((5760 + i*10)) --out udp:127.0.0.1:$((9010 + i*10)) --daemon"
 	sleep 1
