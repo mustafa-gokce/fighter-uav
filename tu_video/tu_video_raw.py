e62117a349241374bfa8c122b0303740e3da3a28
m <mustafagokce009@gmail.com>
2021-12-15 17:40:22 +0300

    tu_video: created pub/sub utilities and compressed the streams

--- tu_video/tu_video_raw.py
+++ tu_video/tu_video_raw.py
@@ -1,21 +1,10 @@
 import time
-import zmq
 import cv2
+import tu_video_utils
 import tu_settings
 
-
-# publish video stream to endpoints
-def tu_video_pub(socket, img, data, flags=0):
-    md = dict(dtype=str(img.dtype), shape=img.shape)
-    socket.send_json(data, flags | zmq.SNDMORE)
-    socket.send_json(md, flags | zmq.SNDMORE)
-    return socket.send(img, flags)
-
-
 # create socket
-pub_context = zmq.Context()
-pub_socket = pub_context.socket(zmq.PUB)
-pub_socket.bind("tcp://*:" + str(tu_settings.tu_video_stream_port_local_raw))
+pub_socket = tu_video_utils.tu_video_socket_pub(tu_settings.tu_video_stream_port_local_raw)
 
 # create capture device
 capture_device = cv2.VideoCapture("http://{0}:{1}/stream.mjpg".format(tu_settings.tu_video_stream_ip_local,
@@ -32,10 +21,7 @@ while True:
     success, image_frame = capture_device.read()
 
     # publish video stream to endpoints
-    tu_video_pub(pub_socket, image_frame, {"loop_count": loop_count})
-
-    # print counter for debugging
-    print("seq:", loop_count)
+    tu_video_utils.tu_video_pub(pub_socket, image_frame, {"loop_count": loop_count})
 
     # update counter
     loop_count += 1
