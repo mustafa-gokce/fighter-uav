b25ac77412ba0f1525b01c0379d405ef14bdc9ee
m <mustafagokce009@gmail.com>
2021-12-15 15:12:14 +0300

    tu_panel: first working video stream in panel

--- tu_settings.py
+++ tu_settings.py
@@ -2,10 +2,17 @@
 tu_vehicle_ip = "192.168.1.201"
 
 # settings for video streaming
-tu_video_stream_port = 2609
+tu_video_stream_ip_remote = tu_vehicle_ip
+tu_video_stream_ip_local = "127.0.0.1"
+tu_video_stream_id_device = -1
+tu_video_stream_port_remote_raw = 9000
+tu_video_stream_port_local_raw = 9001
+tu_video_stream_port_local_judge = 9002
+tu_video_stream_port_local_osd = 9003
 tu_video_stream_width = 1280
 tu_video_stream_height = 720
 tu_video_stream_fps = 41
+tu_video_stream_delay = 2
 
 # settings for judge server
 tu_server_ip = "127.0.0.25"
