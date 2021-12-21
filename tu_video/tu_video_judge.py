8d5d10e348b784bee0551d346ee208ae920aa431
m <mustafagokce009@gmail.com>
2021-12-21 21:18:32 +0300

    tu_video: added overlays for judge and OSD frames

--- tu_video/tu_video_judge.py
+++ tu_video/tu_video_judge.py
@@ -2,6 +2,13 @@ import cv2
 import tu_video_utils
 import tu_settings
 
+# drawing related settings
+hit_area_top_left = (int(tu_settings.tu_video_stream_width * 0.25), int(tu_settings.tu_video_stream_height * 0.1))
+hit_area_bottom_right = (tu_settings.tu_video_stream_width - hit_area_top_left[0],
+                         tu_settings.tu_video_stream_height - hit_area_top_left[1])
+hit_area_color = (191, 64, 191)
+hit_area_thickness = 5
+
 # create sub socket
 sub_socket = tu_video_utils.tu_video_create_sub(tu_settings.tu_video_stream_ip_local,
                                                 tu_settings.tu_video_stream_port_local_raw)
@@ -19,7 +26,7 @@ while True:
     my_data["judge_loop_count"] = loop_count
 
     # manipulate frame
-    my_image = cv2.flip(my_image, 0)
+    my_image = cv2.rectangle(my_image, hit_area_top_left, hit_area_bottom_right, hit_area_color, hit_area_thickness)
 
     # publish video stream to endpoints
     tu_video_utils.tu_video_pub(pub_socket, my_image, my_data)
--- tu_video/tu_video_osd.py
+++ tu_video/tu_video_osd.py
@@ -2,6 +2,18 @@ import cv2
 import tu_video_utils
 import tu_settings
 
+# drawing related settings
+hit_area_top_left = (int(tu_settings.tu_video_stream_width * 0.25), int(tu_settings.tu_video_stream_height * 0.1))
+hit_area_bottom_right = (tu_settings.tu_video_stream_width - hit_area_top_left[0],
+                         tu_settings.tu_video_stream_height - hit_area_top_left[1])
+hit_area_color = (191, 64, 191)
+hit_area_thickness = 5
+osd_font = cv2.FONT_HERSHEY_SIMPLEX
+osd_font_scale = 1
+osd_font_color = (0, 255, 0)
+osd_font_thickness = 2
+osd_font_line_type = cv2.LINE_AA
+
 # create sub socket
 sub_socket = tu_video_utils.tu_video_create_sub(tu_settings.tu_video_stream_ip_local,
                                                 tu_settings.tu_video_stream_port_local_raw)
@@ -19,7 +31,11 @@ while True:
     my_data["osd_loop_count"] = loop_count
 
     # manipulate frame
-    my_image = cv2.flip(my_image, 1)
+    my_image = cv2.rectangle(my_image, hit_area_top_left, hit_area_bottom_right, hit_area_color, hit_area_thickness)
+    my_image = cv2.putText(my_image, "TEST UCUSU", (5, 710),
+                           osd_font, osd_font_scale, osd_font_color, osd_font_thickness, osd_font_line_type)
+    my_image = cv2.putText(my_image, "TEKNOFEST 2022", (1005, 710),
+                           osd_font, osd_font_scale, osd_font_color, osd_font_thickness, osd_font_line_type)
 
     # publish video stream to endpoints
     tu_video_utils.tu_video_pub(pub_socket, my_image, my_data)
