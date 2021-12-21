88d826a826b194f9bfb1dcad937328bb7c6f2fae
m <mustafagokce009@gmail.com>
2021-12-21 20:47:28 +0300

    tu_core: added panel test started and ender scripts

---
+++ tu_core/tu_panel_test_end.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# get inside the log directory
+mkdir -p "$HOME"/test-ucusu/fighter-uav/logs
+cd "$HOME"/test-ucusu/fighter-uav/logs || exit 1
+
+screen -X -S tu_test_video quit
+screen -X -S tu_video_raw quit
+screen -X -S tu_video_judge quit
+screen -X -S tu_video_osd quit
+screen -X -S tu_panel quit
