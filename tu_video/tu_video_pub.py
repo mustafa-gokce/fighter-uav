1ec98d96487e30610b14b4148829bf68b04630d1
m <mustafagokce009@gmail.com>
2021-12-21 11:59:50 +0300

    tu_video: changed pub/sub function names

--- tu_video/tu_video_pub.py
+++ tu_video/tu_video_pub.py
@@ -5,7 +5,7 @@ from tu_video import tu_video_utils
 import tu_settings
 
 # create pub socket
-pub_socket = tu_video_utils.tu_video_socket_pub(tu_settings.tu_video_stream_port_remote_raw)
+pub_socket = tu_video_utils.tu_video_create_pub(tu_settings.tu_video_stream_port_remote_raw)
 
 # initiate camera object with context manager
 with picamera.PiCamera(resolution="{0}x{1}".format(tu_settings.tu_video_stream_width,
@@ -24,7 +24,7 @@ with picamera.PiCamera(resolution="{0}x{1}".format(tu_settings.tu_video_stream_w
         image_frame = frame.array
 
         # publish video stream to endpoints
-        tu_video_utils.tu_video_pub(pub_socket, image_frame, {"loop_count": loop_count})
+        tu_video_utils.tu_video_pub(pub_socket, image_frame, {"pub_loop_count": loop_count})
 
         # update loop counter
         loop_count += 1
