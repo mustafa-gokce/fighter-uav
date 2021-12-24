f464681ec87510f712b761fb8f968e94ad840d68
m <mustafagokce009@gmail.com>
2021-12-24 14:08:30 +0300

    tu_video: rpi compat

--- tu_video/tu_video_raw.py
+++ tu_video/tu_video_raw.py
@@ -1,9 +1,10 @@
+import cv2
 import tu_video_utils
 import tu_settings
 
-# create sub socket
-sub_socket = tu_video_utils.tu_video_create_sub(tu_settings.tu_video_stream_ip_remote,
-                                                tu_settings.tu_video_stream_port_remote_raw)
+# create capture device
+capture_device = cv2.VideoCapture("http://{0}:{1}/stream.mjpg".format(tu_settings.tu_video_stream_ip_remote,
+                                                                      tu_settings.tu_video_stream_port_remote_raw))
 
 # create pub socket
 pub_socket = tu_video_utils.tu_video_create_pub(tu_settings.tu_video_stream_port_local_raw)
@@ -11,11 +12,12 @@ pub_socket = tu_video_utils.tu_video_create_pub(tu_settings.tu_video_stream_port
 # do below always
 loop_count = 0
 while True:
-    # receive video stream from source
-    my_data, my_image = tu_video_utils.tu_video_sub(sub_socket)
+
+    # capture frame
+    success, my_image = capture_device.read()
 
     # manipulate data
-    my_data["raw_loop_count"] = loop_count
+    my_data = {"raw_loop_count": loop_count}
 
     # publish video stream to endpoints
     tu_video_utils.tu_video_pub(pub_socket, my_image, my_data)
