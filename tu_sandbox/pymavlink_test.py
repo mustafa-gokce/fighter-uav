6b5293633be2d720db3d0524686e05eb4ea61f2a
m <mustafagokce009@gmail.com>
2021-12-27 17:11:19 +0300

    tu_sandbox: found mavlink dialects

--- tu_sandbox/pymavlink_test.py
+++ tu_sandbox/pymavlink_test.py
@@ -1,2 +1,5 @@
 from pymavlink import mavutil
 master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+from pymavlink.dialects.v20 import all as mavlink2
+mavlink2.MAVLink_statustext_message(mavlink2.MAV_SEVERITY_INFO, "HELLO")
+mavlink2.MAVLink_mission_set_current_message()
