c7c58e5cbdfbe93834e32d02e370f947548bb709
m <mustafagokce009@gmail.com>
2022-02-17 23:40:12 +0300

    login to judge server

--- tu_telem/tu_proxy_telem.sh
+++ tu_telem/tu_proxy_telem.sh
@@ -5,8 +5,4 @@ mkdir -p "$HOME"/test-ucusu/fighter-uav/logs
 cd "$HOME"/test-ucusu/fighter-uav/logs || exit 1
 
 # start telemetry stream
-for i in $(seq 0 0)
-do
-	screen -S telemetry_proxy$((i + 1)) -d -m bash -c "mavproxy.py --aircraft uav$((i + 1)) --master udp:127.0.0.1:$((9010 + i*10)) --out udp:127.0.0.1:$((10010 + i*10)) --out udp:127.0.0.1:$((20010 + i*10)) --out udp:127.0.0.1:$((30010 + i*10)) --daemon"
-	sleep 1
-done
+screen -S telemetry_proxy -d -m bash -c "mavproxy.py --aircraft zurna --master udp:127.0.0.1:9010 --out udp:127.0.0.1:10010 --out udp:127.0.0.1:20010 --out udp:127.0.0.1:30010 --daemon"
--- tu_test/tu_test_sitl.sh
+++ tu_test/tu_test_sitl.sh
@@ -5,8 +5,5 @@ mkdir -p "$HOME"/test-ucusu/fighter-uav/logs
 cd "$HOME"/test-ucusu/fighter-uav/logs || exit 1
 
 # start simulations
-for i in $(seq 0 4)
-do
-	screen -S simulation_firmware$((i + 1)) -d -m bash -c "$HOME/test-ucusu/fighter-uav/tu_test/arduplane -S -I$i --model plane --speedup 1 --defaults $HOME/test-ucusu/fighter-uav/tu_test/plane.parm"
-	sleep 1
-done
+screen -S simulation_firmware -d -m bash -c "$HOME/test-ucusu/fighter-uav/tu_test/arduplane -S -I0 --model plane --speedup 1 --defaults $HOME/test-ucusu/fighter-uav/tu_test/plane.parm"
+screen -S simulation_proxy -d -m bash -c "mavproxy.py --aircraft zurna --master tcp:127.0.0.1:5760 --out udp:127.0.0.1:9010 --daemon"
