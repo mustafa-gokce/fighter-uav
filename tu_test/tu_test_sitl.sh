f918d239bfeb7a8f592a58909e0f24eff550cf07
m <mustafagokce009@gmail.com>
2022-02-19 17:08:55 +0300

    comments on interop objects

--- tu_test/tu_test_sitl.sh
+++ tu_test/tu_test_sitl.sh
@@ -5,5 +5,5 @@ mkdir -p "$HOME"/test-ucusu/fighter-uav/logs
 cd "$HOME"/test-ucusu/fighter-uav/logs || exit 1
 
 # start simulations
-screen -S simulation_firmware -d -m bash -c "$HOME/test-ucusu/fighter-uav/tu_test/arduplane -S -I0 --model plane --speedup 1 --defaults $HOME/test-ucusu/fighter-uav/tu_test/plane.parm"
-screen -S simulation_proxy -d -m bash -c "mavproxy.py --aircraft zurna --master tcp:127.0.0.1:5760 --out udp:127.0.0.1:9010 --daemon"
+screen -S tu_simulation_firmware -d -m bash -c "$HOME/test-ucusu/fighter-uav/tu_test/arduplane -S -I0 --model plane --speedup 1 --defaults $HOME/test-ucusu/fighter-uav/tu_test/plane.parm"
+screen -S tu_simulation_proxy -d -m bash -c "mavproxy.py --aircraft zurna --master tcp:127.0.0.1:5760 --out udp:127.0.0.1:9010 --daemon"
