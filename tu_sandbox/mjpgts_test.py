41fb41606ea84822331835fa755f17b5b8006d5f
m <mustafagokce009@gmail.com>
2022-02-21 15:32:09 +0300

    video publisher test code

--- tu_sandbox/mjpgts_test.py
+++ tu_sandbox/mjpgts_test.py
@@ -2,3 +2,4 @@
 # ffplay -analyzeduration 1 -fflags -nobuffer -probesize 32 -sync ext -fast udp://127.0.0.1:23000
 # sudo modprobe v4l2loopback video_nr=26
 # sudo apt install v4l2loopback-dkms
+# ffplay /dev/video26
